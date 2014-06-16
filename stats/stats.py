454aff6d4cdfcc20faf1cdcb2c2bc3bd65b8de80
Diogo Silva <o.diogosilva@gmail.com>
2014-06-16 11:13:07 +0100

    Added the main stats module file

---
+++ stats/stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#
+#  Copyright 2012 Unknown <diogo@arch>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+
+
+__author__ = "Diogo N. Silva"
+__copyright__ = "Diogo N. Silva"
+__credits__ = ["Diogo N. Silva"]
+__license__ = "GPL"
+__version__ = "0.1.0"
+__maintainer__ = "Diogo N. Silva"
+__email__ = "o.diogosilva@gmail.com"
+__status__ = "Prototype"
