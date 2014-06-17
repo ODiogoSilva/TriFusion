8c6bd1c9386d38e3eeeba2394c7bb1892d3c3663
Diogo Silva <o.diogosilva@gmail.com>
2014-06-17 16:41:26 +0100

    First running version of TriStats with the simple option of generating a simple table with basic phylogenetic statistics

--- TriStats.py
+++ TriStats.py
@@ -26,7 +26,40 @@
 ## TODO [URGENT]: This was originally in python2. I'll need to port to python3
 
 import argparse
+from stats import stats
 
+parser = argparse.ArgumentParser(description="Filters alignment files and creates statistics and graphics for "
+											 "alignments")
+parser.add_argument("-in",dest="infile",nargs="+",required=True,help="Provide the input files")
+parser.add_argument("-o", dest="project_name", required=True, help="Provide a name for the project")
+
+modes = parser.add_argument_group("Report options")
+modes.add_argument("-f", dest="full_report",action="store_const",const=True, help="Generate full report")
+modes.add_argument("-m", dest="mode", nargs="+", choices=["1"], help="Specify which report(s): \n\t\t1: Basic " \
+																	"phylogenetic information")
+
+arg = parser.parse_args()
+
+def main():
+
+	# Arguments
+	input_list = arg.infile
+	project = arg.project_name
+
+	# Determine the mode
+	if arg.full_report:
+		mode = "all"
+	else:
+		mode = arg.mode
+
+	if "1" in mode or mode == "all":
+
+		report = stats.MultiReport(input_list)
+
+		# Defining output file name based on project
+		output_file = "%s_basic_table.csv" % project
+
+		report.report_table(output_file)
 
 
 if __name__ == '__main__':
