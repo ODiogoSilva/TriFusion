7eb6885f25f088ef71d7faf985b41761332fed39
Diogo Silva <o.diogosilva@gmail.com>
2014-06-18 16:58:10 +0100

    Added missing data profile classes in UniReport and MultiReport classes

--- stats/stats.py
+++ stats/stats.py
@@ -22,6 +22,7 @@
 
 from process.alignment import Alignment
 from collections import OrderedDict
+import pygal
 
 class UniReport(Alignment):
 	""" This will create a report object for single alignments and will inherit from the Alignment class. As with its
@@ -70,6 +71,24 @@ class UniReport(Alignment):
 
 		return indel, indel_perc, variable, parsimonious
 
+	def missing_data(self):
+		"""
+		Returns a dictionary with the missing data information for each species. Each dictionary entry will be:
+		"taxon":[missing_int, gap_int, gap_int+missing_int]
+		"""
+
+		contents_missing_data = OrderedDict()
+		# contents_missing_data = dict((taxa, []) for taxa in self.iter_taxa())  # Creates the base dictionary with
+		# taxa names as keys and empty lists as values
+
+		for taxon, sequence in self.alignment.items():
+			gap_value = sequence.count("-")
+			missing_value = sequence.count(self.sequence_code[1])
+			contents_missing_data[taxon] = [gap_value, missing_value, gap_value + missing_value]
+
+		return contents_missing_data
+
+
 class MultiReport():
 	""" This will create a report object for multiple alignments. Its most basic instance will be a list of UniReport
 	objects
@@ -83,8 +102,26 @@ class MultiReport():
 
 			self.report_list.append(report_object)
 
+	def _get_species_set(self):
+		"""
+		:return: A list containing the unique taxa names from all alignment files
+		"""
 
-	def report_table (self, output_file):
+		species_set = set()
+
+		for report in self.report_list:
+
+			current_taxa = report.iter_taxa()
+			if set(current_taxa) != species_set:
+				species_set = set(list(species_set) + current_taxa)
+
+		return list(species_set)
+
+	def report_table(self, output_file):
+		"""
+		:param output_file: output file name string
+		Generates a csv table containing basic phylogenetic statistics for each alignment
+		"""
 
 		output_handle = open(output_file,"w")
 		output_handle.write("Gene; Number of sites; Number of indels; Percentage of missing data; Variable sites; "
@@ -105,6 +142,83 @@ class MultiReport():
 			output_handle.write("%s; %s; %s; %s; %s; %s\n" % (gene, vals[0], vals[1], vals[2], vals[3], vals[4]))
 
 		output_handle.close()
+
+	def species_missing_data(self, table=False, plot=False, output_file=None):
+		"""
+		:param table: Boolean. True will generate a csv table with information on the missing data for each species
+		:param plot: Boolean. True will generate a stacked bar plot with information on the missing data for each
+		species
+		Depending on the table and plot argument values, this will generate a table and/or a plot with information on
+		the missing data for each species
+		"""
+
+		taxa_set = self._get_species_set()
+		missing_data_contents = dict((taxa, [0,0,0]) for taxa in taxa_set)
+		alignment_length = 0
+
+		for report in self.report_list:
+			report_missing_data = report.missing_data()
+			alignment_length += report.locus_length
+
+			for taxon, vals in report_missing_data.items():
+				missing_data_contents[taxon] = [x + y for x, y in zip(missing_data_contents[taxon], vals)]
+
+		# Convert missing_data_contents to list and sort by missing data
+		data_list = [(taxon, [vals[0], vals[1], vals[2]]) for taxon, vals in missing_data_contents.items()]
+		sorted_data_list = sorted(data_list, key=lambda  x: x[1][2], reverse=True)
+
+		# Table
+		if table is True:
+
+			try:
+				output_handle = open(output_file,"w")
+			except TypeError:
+				print ("TypeError: Please specify an output file name")
+				raise SystemExit
+
+			output_handle.write("Taxon; Gap chars; Missing chars; Total missing chars; Effective chars; Total chars\n")
+
+			for taxon, vals in sorted_data_list:
+				output_handle.write("%s; %s; %s; %s; %s; %s\n" % (taxon,
+																  vals[0],
+																  vals[1],
+																  vals[2],
+																  alignment_length - vals[2],
+																  alignment_length))
+			else:
+				output_handle.close()
+
+		# Plot
+		if plot is True:
+
+			missing_bar_chart = pygal.StackedBar(x_label_rotation=90,width=1200, legend_at_bottom=True, height=800,
+										  label_font_size=8,legend_font_size=20,margin=50,major_label_font_size=10)
+			missing_bar_chart.title = "Character missing data per species"
+			missing_bar_chart.x_labels = [taxon[0] for taxon in sorted_data_list]
+
+			gap_list = []
+			missing_list =[]
+			character_list = []
+
+			for taxon, vals in sorted_data_list:
+				# Adjusting missing data values to be in proportion
+				gap_proportion = float(vals[0]) / float(alignment_length)
+				missing_proportion = float(vals[1]) / float(alignment_length)
+				character_proportion = (float(alignment_length)-float(vals[2])) / float(alignment_length)
+
+				print (taxon, gap_proportion, missing_proportion, character_proportion)
+
+				# Creating chart variables
+				gap_list.append({"value": gap_proportion, "label": str(vals[0])})
+				missing_list.append({"value": missing_proportion, "label": str(vals[1])})
+				character_list.append({"value": character_proportion, "label": str(alignment_length-vals[2])})
+
+			# Adding chart variables
+			missing_bar_chart.add("Gaps", gap_list)
+			missing_bar_chart.add("Missing data", missing_list)
+			missing_bar_chart.add("Characters", character_list)
+
+			return missing_bar_chart
 
 __author__ = "Diogo N. Silva"
 __copyright__ = "Diogo N. Silva"
