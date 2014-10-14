1288a8b7a4a4276b516ee777e2f32f705a759cd8
Diogo Silva <o.diogosilva@gmail.com>
2014-10-14 12:35:12 +0100

    Compliance to PEP8 guidelines

--- TriSeq.py
+++ TriSeq.py
@@ -134,7 +134,7 @@ def main_parser(alignment_list):
 	else:
 		sequence_format = "interleave"
 
-	print (conversion, arg.outfile)
+	print(conversion, arg.outfile)
 	# Defining output file name
 	if conversion is None and arg.outfile is not None:
 		outfile = "".join(arg.outfile)
