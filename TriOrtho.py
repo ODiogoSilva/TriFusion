bf082b5db354d247c85a4122813b0b1ea7c6666a
Diogo Silva <o.diogosilva@gmail.com>
2014-11-13 21:11:40 +0000

    Converted indents and wrapper lines according to PEP8

--- TriOrtho.py
+++ TriOrtho.py
@@ -24,29 +24,35 @@
 #  Last update:
 #
 
-__author__ = 'DiogoNSilva'
-
 import argparse
 from ortho import OrthomclToolbox as OT
 
-parser = argparse.ArgumentParser(description="Toolbox to analyse and filter OrthoMCL group files")
+parser = argparse.ArgumentParser(description="Toolbox to analyse and filter "
+                                 "OrthoMCL group files")
 
-parser.add_argument("-in", dest="infile", nargs="+", help="Provide the OrthoMCL group file(s)")
-parser.add_argument("-t1", "--gene-threshold", dest="gene_threshold", nargs=1, default=1, help="Provide the maximum "
-					"threshold of gene copy numbers per species that should be allowed")
-parser.add_argument("-t2", "--species-threshold", dest="species_threshold", nargs=1, default=200, help="Provide the "
-					"minimum number of species that should be allowed.")
-parser.add_argument("-e", dest="export", action="store_const", const=True, help="Exports the filtered groups into a "
-					"new file")
-parser.add_argument("-s", dest="stats", choices=["1", "2"], help="Use the available choices to perform statistical "
-					"analyses or summaries of the groups file. 1: Generates basic summary statistics on the number of "
-					"clusters and sequences of the groups file; 2: Generates csv table with the number of clusters "
-					"containing paralogs per species.")
-parser.add_argument("-g2f", dest="groups2fasta", help="Retrieves the sequences of each cluster to a single file per "
-					"cluster. The BLAST database must be provided with this option")
-parser.add_argument("-p", dest="pipeline", nargs="*", choices=["1"], help="TriOrtho can be used in pipeline format to "
-					"perform sequential steps that users most often use. 1: Filter the original groups file and "
-					"retrieve the Fasta sequences. ")
+parser.add_argument("-in", dest="infile", nargs="+", help="Provide the OrthoMCL"
+                    " group file(s)")
+parser.add_argument("-t1", "--gene-threshold", dest="gene_threshold", nargs=1,
+                    default=1, help="Provide the maximum threshold of gene "
+                    "copy numbers per species that should be allowed")
+parser.add_argument("-t2", "--species-threshold", dest="species_threshold",
+                    nargs=1, default=200, help="Provide the minimum number of "
+                    "species that should be allowed.")
+parser.add_argument("-e", dest="export", action="store_const", const=True,
+                    help="Exports the filtered groups into a new file")
+parser.add_argument("-s", dest="stats", choices=["1", "2"], help="Use the "
+                    "available choices to perform statistical analyses or "
+                    "summaries of the groups file. 1: Generates basic summary"
+                    " statistics on the number of clusters and sequences of "
+                    "the groups file; 2: Generates csv table with the number of"
+                    " clusters containing paralogs per species.")
+parser.add_argument("-g2f", dest="groups2fasta", help="Retrieves the sequences"
+                    " of each cluster to a single file per cluster. The BLAST"
+                    " database must be provided with this option")
+parser.add_argument("-p", dest="pipeline", nargs="*", choices=["1"],
+                    help="TriOrtho can be used in pipeline format to perform "
+                    "sequential steps that users most often use. 1: Filter the"
+                    " original groups file and retrieve the Fasta sequences. ")
 
 arg = parser.parse_args()
 
@@ -55,44 +61,45 @@ arg = parser.parse_args()
 
 def main():
 
-	# Arguments
-	groups_file = arg.infile
-	gene_threshold = int(arg.gene_threshold[0])
-	species_threshold = int(arg.species_threshold[0])
-	pipeline_mode = arg.pipeline
-	blast_database = arg.groups2fasta
+    # Arguments
+    groups_file = arg.infile
+    gene_threshold = int(arg.gene_threshold[0])
+    species_threshold = int(arg.species_threshold[0])
+    pipeline_mode = arg.pipeline
+    blast_database = arg.groups2fasta
 
-	if len(groups_file) == 1:
+    if len(groups_file) == 1:
 
-		group_file = groups_file[0]
-		group_object = OT.Group(group_file, gene_threshold, species_threshold)
+        group_file = groups_file[0]
+        group_object = OT.Group(group_file, gene_threshold, species_threshold)
 
-		if pipeline_mode:
+        if pipeline_mode:
 
-			if "1" in pipeline_mode:
-				group_object.update_filtered_group()
-				group_object.retrieve_fasta(blast_database)
+            if "1" in pipeline_mode:
+                group_object.update_filtered_group()
+                group_object.retrieve_fasta(blast_database)
 
-		else:
+        else:
 
-			if arg.export:
-				group_object.export_filtered_group()
+            if arg.export:
+                group_object.export_filtered_group()
 
-			if arg.stats:
-				#if "1" in arg.stats:
+            if arg.stats:
+                #if "1" in arg.stats:
 
-				if "2" in arg.stats:
-					group_object.export_filtered_group()
+                if "2" in arg.stats:
+                    group_object.export_filtered_group()
 
-			if blast_database:
-				group_object.retrieve_fasta(blast_database)
+            if blast_database:
+                group_object.retrieve_fasta(blast_database)
 
-	else:
-		multiple_groups_object = OT.MultiGroups(groups_file, gene_threshold, species_threshold)
+    else:
+        multiple_groups_object = OT.MultiGroups(groups_file, gene_threshold,
+                                                species_threshold)
 
-		if arg.stats:
-			if "1" in arg.stats:
-				multiple_groups_object.basic_multigroup_statistics()
+        if arg.stats:
+            if "1" in arg.stats:
+                multiple_groups_object.basic_multigroup_statistics()
 
 
 main()
