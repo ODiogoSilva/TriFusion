1b82f2bdb1d399ff400ff6a73514a4bdb53f64d3
Diogo Silva <o.diogosilva@gmail.com>
2014-11-13 18:52:37 +0000

    Corrected wrapped text according to PEP8

--- process/data.py
+++ process/data.py
@@ -25,8 +25,10 @@
 
 
 class Partitions ():
-    """ The Partitions class is used to parse and work with partition files. Currently the supported input formats are
-    RAxML partition files and Nexus chartset blocks. To initialize a class, only a file partition file is required. """
+    """ The Partitions class is used to parse and work with partition files.
+    Currently the supported input formats are RAxML partition files and Nexus
+     chartset blocks. To initialize a class, only a file partition file is
+      required. """
     def __init__(self, partition_file, model_nexus="LG"):
 
         self.model_nexus = model_nexus
@@ -34,7 +36,8 @@ class Partitions ():
         self.partitions = self._get_partitions(partition_file)
 
     def _get_format(self, partition_file):
-        """ Tries to guess the format of the partition file (Whether it is Nexus of RAxML's) """
+        """ Tries to guess the format of the partition file (Whether it is
+         Nexus of RAxML's) """
         file_handle = open(partition_file)
 
         # Skips first empty lines, if any
@@ -51,9 +54,11 @@ class Partitions ():
         return partition_format
 
     def _get_partitions(self, partitions_file):
-        """ This function parses a file containing partitions. Supports partitions files similar to RAxML's and NEXUS
-        charset blocks. The NEXUS file, however, must only contain the charset block. The model_nexus argument provides
-        a namespace for the model variable in the nexus format, since this information is not present in the file.
+        """ This function parses a file containing partitions. Supports
+        partitions files similar to RAxML's and NEXUS charset blocks. The
+        NEXUS file, however, must only contain the charset block. The
+        model_nexus argument provides a namespace for the model variable in
+        the nexus format, since this information is not present in the file.
         However, it assures consistency on the Partition object """
 
         # Get the format of the partition file
@@ -69,36 +74,45 @@ class Partitions ():
                 partition_name = fields[1].split("=")[0].strip()
                 partition_range_temp = fields[1].split("=")[1]
                 partition_range = partition_range_temp.strip().split("-")
-                partition_storage.append((model, partition_name, partition_range))  # Format of partition storage: [
-                # "str","str",["str","str"]]
+                # Format of partition storage: [# "str","str",["str","str"]]
+                partition_storage.append((model,
+                                          partition_name,
+                                          partition_range))
 
         elif self.partition_format == "nexus":
             for line in part_file:
                 fields = line.split("=")
                 partition_name = fields[0].split()[1].strip()
                 partition_range = fields[1].replace(";", "").strip().split("-")
-                partition_storage.append((self.model_nexus, partition_name, partition_range))  # Format of partition
-                # storage: ["str", str", ["str","str"]]
+                # Format of partition storage: ["str", str", ["str","str"]]
+                partition_storage.append((self.model_nexus,
+                                          partition_name,
+                                          partition_range))
 
         return partition_storage
 
     def write_to_file(self, output_format, output_file, model="LG"):
-        """ Writes the Partitions object into an output file according to the output_format. The supported output
-        formats are RAxML and Nexus. The model option is for the RAxML format """
+        """ Writes the Partitions object into an output file according to the
+         output_format. The supported output formats are RAxML and Nexus.
+         9The model option is for the RAxML format """
 
         if output_format == "raxml":
             outfile_handle = open(output_file + ".part.File", "w")
             for part in self.partitions:
                 partition_name = part[0]
                 partition_range = "-".join([x for x in part[1]])
-                outfile_handle.write("%s, %s = %s\n" % (model, partition_name, partition_range))
+                outfile_handle.write("%s, %s = %s\n" % (model,
+                                                        partition_name,
+                                                        partition_range))
 
             outfile_handle.close()
 
         elif output_format == "nexus":
             outfile_handle = open(output_file + ".charset", "w")
             for part in self.partitions:
-                outfile_handle.write("charset %s = %s;\n" % (part[1], "-".join(part[2])))
+                outfile_handle.write("charset %s = %s;\n" % (
+                                     part[1],
+                                     "-".join(part[2])))
 
             outfile_handle.close()
 
@@ -109,25 +123,29 @@ class Zorro ():
 
     def __init__(self, alignment_list, suffix="_zorro.out"):
 
-        def zorro2rax(self, alignment_list):
-            """ Function that converts the floating point numbers contained in the original zorro output files into
-            integers that can be interpreted by RAxML. If multiple alignment files are provided, it also concatenates
-            them in the same order """
+        def zorro2rax(alignment_list):
+            """ Function that converts the floating point numbers contained
+            in the original zorro output files into integers that can be
+            interpreted by RAxML. If multiple alignment files are provided,
+            it also concatenates them in the same order """
             weigths_storage = []
             for alignment_file in alignment_list:
-                zorro_file = alignment_file.split(".")[0]+self.suffix  # This assumes that the prefix of the
+                # This assumes that the prefix of the
+                zorro_file = alignment_file.split(".")[0] + self.suffix
                 # alignment file is shared with the corresponding zorro file
                 zorro_handle = open(zorro_file)
-                weigths_storage += [round(float(weigth.strip())) for weigth in zorro_handle]
+                weigths_storage += [round(float(weigth.strip())) for
+                                    weigth in zorro_handle]
             return weigths_storage
 
         self.suffix = suffix
         self.weigth_values = zorro2rax(alignment_list)
 
     def write_to_file(self, output_file):
-        """ Creates a concatenated file with the zorro weights for the corresponding alignment files """
-        outfile = output_file+"_zorro.out"
-        outfile_handle = open(outfile,"w")
+        """ Creates a concatenated file with the zorro weights for the
+        corresponding alignment files """
+        outfile = output_file + "_zorro.out"
+        outfile_handle = open(outfile, "w")
         for weigth in self.weigth_values:
             outfile_handle.write("%s\n" % weigth)
         outfile_handle.close()
