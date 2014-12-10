bdb77d4dc985cc36504a2a36ea7f423b8e25353e
Diogo Silva <o.diogosilva@gmail.com>
2014-12-10 15:41:30 +0000

    Skeleton GUI app

--- TriFusion.py
+++ TriFusion.py
@@ -23,3 +23,17 @@
 #  Version:
 #  Last update:
 #
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
