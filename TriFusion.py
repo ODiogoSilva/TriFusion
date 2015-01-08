906137ecbd2211bd8e998393b6ebe2255bfa4402
Diogo Silva <o.diogosilva@gmail.com>
2015-01-08 12:24:30 +0000

    [Bug Fix]: Fixed bookmark button duplication when returning to the file chooser screen

--- TriFusion.py
+++ TriFusion.py
@@ -297,12 +297,12 @@ class TriFusionApp(App):
         else:
             self.screen = Builder.load_file(self.available_screens[idx])
 
-        # If the screen to be loaded is the filechooser, set the home path as
-        # the default
-        if self.available_screens[idx].split("/")[-1] == "fc.kv":
-            self.screen.ids.icon_view_tab.path = self.home_path
-            # Initialize bookmarks
-            self.init_bookmark()
+            # If the screen to be loaded is the filechooser, set the home path
+            #  as the default
+            if self.available_screens[idx].split("/")[-1] == "fc.kv":
+                self.screen.ids.icon_view_tab.path = self.home_path
+                # Initialize bookmarks
+                self.init_bookmark()
 
         return self.screen
 
@@ -383,7 +383,7 @@ class TriFusionApp(App):
 
         :param bk: string. bookmark file path
         """
-
+        print("here")
         bookmark_name = bk.split("/")[-1]
         # Define bookmark button
         bt = Button(text=bookmark_name, id=bk,
