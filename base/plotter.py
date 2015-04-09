f0c462115c0618c405b9c0fea2d4d0ca3bfea9fd
Diogo Silva <o.diogosilva@gmail.com>
2015-04-09 18:45:26 +0100

    Major redesign of bar plot function. It now accepts multiple data lists to be shown in bar groups, automatically sets the appropriate legend and changed some colors. Also added a 10 color list to be used by plotter functions.

--- base/plotter.py
+++ base/plotter.py
@@ -25,12 +25,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+Set of 10 easily distinguishable colors that will be used when generating plots
+to ensure consistency in color usage. In case more than 10 colors are required
+(ugh...) they will be randomly generated henceforth.
+"""
+clr_list = [[0, .53, .66],  # light blue
+            [1, .16, .16],     # light red
+            [.21, .78, .22],  # light green
+            [.83, .55, .37],  # light brown
+            [.55, .37, .83],  # purple
+            [1, .4, 0],     # light orange
+            [.77, .77, .77],  # light grey
+            [.78, .22, .22],  # dark red
+            [.1, .1, .1],     # dark grey
+            [0, .66, 0]]      # green
 
-def bar_plot(data, labels=None, title=None):
+
+def bar_plot(data_list, labels=None, lgd_list=None):
     """
     General purpose bar plot with custom layout. Returns a pyplot object
-    :param data: list/array with data to be plotted
+    :param data_list: list with data to be plotted in list/array type in each
+    entry. Data with two groups would be like [[1.23, .53], [1.55, .12]]
     :param labels: list, containing the labels
+    :param lgd_list: list, The legend string for each data set in data_list
     """
 
     # Use ggpot style
@@ -38,32 +56,62 @@ def bar_plot(data, labels=None, title=None):
 
     fig, ax = plt.subplots()
 
-    # Create bar pot
-    bplt = ax.bar(np.arange(len(data)), data, align="center")
+    # Set axys color
+    for sp in ax.spines:
+        ax.spines[sp].set_color(clr_list[6])
+
+    # Set margin between bar groups
+    margin = 0.1
+
+    # Determine bar group width according to the number of data lists
+    w = (1. - 2. * margin) / len(data_list)
+
+    # Create bar plots
+    for i, d in enumerate(data_list):
+        # Get color from 10 color list. If more than 10 colors are required,
+        # randomly generate new ones
+        try:
+            clr = clr_list[i]
+        except IndexError:
+            clr = np.random.rand(3, 1)
+
+        # Determine position of xdata
+        xdata = np.arange(len(d)) + margin + (i * w)
+        bplt = ax.bar(xdata, d, w, color=clr, label=lgd_list[i])
 
     # Add labels
     if labels:
-        ax.set_xticks(np.arange(len(labels)))
-        ax.set_xticklabels(labels, rotation=45, ha="center")
+        ax.set_xticks(np.arange(len(labels)) + 0.5)
+        ax.set_xticklabels(labels, rotation=45, ha="center", fontweight="bold")
 
     # Set label colors
-    ax.tick_params(axis="x", colors=[.5, .5, .5])
-    ax.tick_params(axis="y", colors=[.5, .5, .5])
+    ax.tick_params(axis="x", colors=[.8, .8, .8])
+    ax.tick_params(axis="y", colors=[.8, .8, .8])
 
-    # Add title
-    plt.title(title, size=23, color=[.4, .4, .4], loc="center")
+    # Add legend
+    lgd = plt.legend(bbox_to_anchor=(1, .5), loc="center left",
+                     fancybox=True, shadow=True)
 
     # Add text to the top of bar with the corresponding value
     for b in bplt:
 
-        xpos = b.get_x() + b.get_width() / 2
-        ypos = b.get_height() * .92
-        ax.text(xpos, ypos, b.get_height(), horizontalalignment="center",
-                weight='bold', color="white", size=14)
+        # If the bar width is large enough to accommodate the text with
+        # horizontal orientation
+        if w >= 0.8:
+            xpos = b.get_x() + b.get_width() / 2
+            ypos = b.get_height() * .92
+            ax.text(xpos, ypos, b.get_height(), horizontalalignment="center",
+                    weight='bold', color="white", size=14)
+        else:
+            xpos = b.get_x() + b.get_width() / 2 * 1.3
+            ypos = b.get_height() * .92
+            ax.text(xpos, ypos, b.get_height(), horizontalalignment="center",
+                    verticalalignment="center", weight='bold', color="white",
+                    size=14, rotation=90)
 
     # Automatically adjust figure size to accomodate all labels
-    plt.tight_layout()
+    # plt.tight_layout()
 
-    return plt
+    return plt, lgd
 
 __author__ = 'diogo'
