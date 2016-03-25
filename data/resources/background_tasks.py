45df51e9ee12766f0c3be379ea5b0c7803b55eec
ODiogoSilva <o.diogosilva@gmail.com>
2016-03-25 13:04:14 +0000

    Added new plot and analysis: Detect species missing data outliers

--- data/resources/background_tasks.py
+++ data/resources/background_tasks.py
@@ -594,7 +594,8 @@ def get_stats_data(aln_obj, stats_idx, active_file_set, active_taxa_set,
                "Alignment length/Polymorphism correlation":
                    aln_obj.length_polymorphism_correlation,
                "Distribution of taxa frequency":
-                   aln_obj.taxa_distribution}
+                   aln_obj.taxa_distribution,
+               "Missing data outliers sp": aln_obj.outlier_missing_data_sp}
 
     if additional_args:
         plot_data = methods[stats_idx](**additional_args)
