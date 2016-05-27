2a1918ec12f3d45f1b19a92b6c0598823bfff600
ODiogoSilva <o.diogosilva@gmail.com>
2016-05-27 12:25:54 +0100

    changed version and build
    Former-commit-id: dcef073d592ad384d7097f3b1f0b37322ae5eff7 [formerly a7c915ad47b02f763b9bd66a3563a62014228042] [formerly fffd0aa82c3815025da4467a7bd2e30d0fc1d003]
    Former-commit-id: 6d6e16771ef8123e678fff9178d2b2c420045eb8
    Former-commit-id: 1271ce78fb9bf7eec47640b81a83237ed3e259f8

--- TriFusion.py
+++ TriFusion.py
@@ -79,8 +79,8 @@ if __name__ == "__main__":
     from base.plotter import *
     from ortho.OrthomclToolbox import MultiGroups
 
-    __version__ = "0.2.1"
-    __build__ = "210516"
+    __version__ = "0.2.3"
+    __build__ = "270516"
     __author__ = "Diogo N. Silva"
     __copyright__ = "Diogo N. Silva"
     __credits__ = ["Diogo N. Silva", "Tiago F. Jesus"]
