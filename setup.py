de24083a0ce7760d2e8c26ac3fdffa50b8cb21e0
ODiogoSilva <o.diogosilva@gmail.com>
2016-07-13 18:19:11 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as f:
 
 setup(
     name="TriFusion",
-    version="0.4.1",
+    version="0.4.11",
     packages=["trifusion",
               "trifusion.base",
               "trifusion.data",
