9fb660b590fbb43fe7f25d131d0972a9205ee0dd
ODiogoSilva <o.diogosilva@gmail.com>
2016-07-19 12:16:28 +0100

    corrected imports
    Former-commit-id: 56bb6e7a414e4e6489a01ad462d9c77782fa7fb0 [formerly a9dd8dce12b81cdfdafaa8bb792ae1a3acfcda23] [formerly 7d6adf4278b5e06dd6cdb65c7e95b08c524fa7fc]
    Former-commit-id: 2554b0a2106b3e96bc7f7ce7a6982b36e65d7ca7
    Former-commit-id: a9e674cb509b7d50a77a28d31ac9d2bc78865923

--- trifusion/ortho/OrthomclToolbox.py
+++ trifusion/ortho/OrthomclToolbox.py
@@ -17,10 +17,14 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
-from process.sequence import Alignment
+try:
+    from process.sequence import Alignment
+    from base.plotter import *
+except ImportError:
+    from trifusion.process.sequence import Alignment
+    from trifusion.base.plotter import *
 
 from collections import OrderedDict
-from base.plotter import *
 import pickle
 import os
 import sqlite3
