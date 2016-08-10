52c4e8a7607b44723f749af67a821bb24f6ad71f
ODiogoSilva <o.diogosilva@gmail.com>
2016-08-10 23:06:19 +0100

    Fixed #221
    Former-commit-id: 133291fc0489ad9ce627e352934ec158f35ae096 [formerly c4ced205508453421aa79e2a922c8bd5913f3367] [formerly f5e47714cb7c14102530b121e9a54dee8cb77c76]
    Former-commit-id: 67021e96cdbb339c02f9923b73d204bd5a54d147
    Former-commit-id: 2fd11c630a4dae50ae34be4aa766b3afef720f4f

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as f:
 
 setup(
     name="TriFusion",
-    version="0.4.20",
+    version="0.4.21",
     packages=["trifusion",
               "trifusion.base",
               "trifusion.data",
