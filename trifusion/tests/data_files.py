19735628af3af68754ad0724411c8a616d2644a7
ODiogoSilva <o.diogosilva@gmail.com>
2016-09-24 19:10:05 +0100

    corrected test file paths
    Former-commit-id: 38907a02911b014f0495d070739ac5ab48e9d389 [formerly 803838a5be33285bb77537f1050802c2b69682c5] [formerly 89b86e0503404c6cce3d65223ff1d6363b8893af]
    Former-commit-id: 2634464cb1f6a1c93cf66e0575e13d3a14b52eb5
    Former-commit-id: fb557e9c0cc7ca4905e924046eacae2848a85e29

--- trifusion/tests/data_files.py
+++ trifusion/tests/data_files.py
@@ -7,148 +7,107 @@ Contains data file paths for testing
 ################################################################################
 
 single_dna = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "BaseConc1.fas"
+    "trifusion/tests/data/BaseConc1.fas"
 ]
 
 concatenated_medium_nexus = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "Base614.nex"
+    "trifusion/tests/data/Base614.nex"
 ]
 
 concatenated_small_phy = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "BaseConcatenation.phy"
+    "trifusion/tests/data/BaseConcatenation.phy"
 ]
 
 concatenated_small_par = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "BaseConcatenation_part.File"
+    "trifusion/tests/data/BaseConcatenation_part.File"
 ]
 
 concatenated_small_parNex = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "BaseConcatenation_part.nex"
+    "trifusion/tests/data/BaseConcatenation_part.nex"
 ]
 
 concatenated_smallCodon_parNex = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "BaseConcatenationCodon_part.nex"
+    "trifusion/tests/data/BaseConcatenationCodon_part.nex"
 ]
 
 dna_data_fas = [
     "/home/diogo/Diogo/Science/Scripts/packages/TriFusion"
     "/trifusion/tests/data/BaseConc1.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc2.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc3.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc4.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc5.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc6.fas",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc7.fas"
+    "trifusion/tests/data/BaseConc2.fas",
+    "trifusion/tests/data/BaseConc3.fas",
+    "trifusion/tests/data/BaseConc4.fas",
+    "trifusion/tests/data/BaseConc5.fas",
+    "trifusion/tests/data/BaseConc6.fas",
+    "trifusion/tests/data/BaseConc7.fas"
 ]
 
 dna_data_phy = [
     "/home/diogo/Diogo/Science/Scripts/packages/TriFusion"
     "/trifusion/tests/data/BaseConc1.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc2.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc3.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc4.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc5.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc6.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc7.phy"
+    "trifusion/tests/data/BaseConc2.phy",
+    "trifusion/tests/data/BaseConc3.phy",
+    "trifusion/tests/data/BaseConc4.phy",
+    "trifusion/tests/data/BaseConc5.phy",
+    "trifusion/tests/data/BaseConc6.phy",
+    "trifusion/tests/data/BaseConc7.phy"
 ]
 
 dna_data_nex = [
     "/home/diogo/Diogo/Science/Scripts/packages/TriFusion"
     "/trifusion/tests/data/BaseConc1.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc2.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc3.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc4.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc5.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc6.nex",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc7.nex"
+    "trifusion/tests/data/BaseConc2.nex",
+    "trifusion/tests/data/BaseConc3.nex",
+    "trifusion/tests/data/BaseConc4.nex",
+    "trifusion/tests/data/BaseConc5.nex",
+    "trifusion/tests/data/BaseConc6.nex",
+    "trifusion/tests/data/BaseConc7.nex"
 ]
 
 dna_data_stc = [
     "/home/diogo/Diogo/Science/Scripts/packages/TriFusion"
     "/trifusion/tests/data/BaseConc1.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc2.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc3.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc4.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc5.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc6.stockholm",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "/data/BaseConc7.stockholm"
+    "trifusion/tests/data/BaseConc2.stockholm",
+    "trifusion/tests/data/BaseConc3.stockholm",
+    "trifusion/tests/data/BaseConc4.stockholm",
+    "trifusion/tests/data/BaseConc5.stockholm",
+    "trifusion/tests/data/BaseConc6.stockholm",
+    "trifusion/tests/data/BaseConc7.stockholm"
 ]
 
 dna_data_loci = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/data/"
-    "c97d5m4p2.loci"
+    "trifusion/tests/data/c97d5m4p2.loci"
 ]
 
 bad_file = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/bad_file.phy"
+    "trifusion/tests/data/bad_file.phy"
 ]
 
 unequal_file = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/unequal_length.phy"
+    "trifusion/tests/data/unequal_length.phy"
 ]
 
 no_data = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/no_data.phy"
+    "trifusion/tests/data/no_data.phy"
 ]
 
 codon_filter = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/codon_filter.phy"
+    "trifusion/tests/data/codon_filter.phy"
 ]
 
 variable_data = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/variable_data1.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/variable_data2.phy",
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/variable_data3.phy"
+    "trifusion/tests/data/variable_data1.phy",
+    "trifusion/tests/data/variable_data2.phy",
+    "trifusion/tests/data/variable_data3.phy"
 ]
 
 gcode_data = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/gcoding.phy"
+    "trifusion/tests/data/gcoding.phy"
 ]
 
 models_nexus_data = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/models.nex"
+    "trifusion/tests/data/models.nex"
 ]
 
 models_codon_nexus_data = [
-    "/home/diogo/Diogo/Science/Scripts/packages/TriFusion/trifusion/tests/"
-    "data/models_codon.nex"
+    "trifusion/tests/data/models_codon.nex"
 ]
