4ea3ff38c3b4f47732a3739ab223b46640814a42
ODiogoSilva <o.diogosilva@gmail.com>
2016-09-24 20:03:55 +0100

    Added test to suite

--- test_runner.py
+++ test_runner.py
@@ -5,6 +5,7 @@ import trifusion.tests.test_partitions as partitions
 import trifusion.tests.test_process_filters as process_filters
 import trifusion.tests.test_process_write as process_write
 import trifusion.tests.test_secondary_ops as secondary_ops
+import trifusion.tests.test_stats as stats
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
@@ -15,6 +16,7 @@ suite.addTests(loader.loadTestsFromModule(partitions))
 suite.addTests(loader.loadTestsFromModule(process_filters))
 suite.addTests(loader.loadTestsFromModule(process_write))
 suite.addTests(loader.loadTestsFromModule(secondary_ops))
+suite.addTests(loader.loadTestsFromModule(stats))
 
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
