4a98ee67ad53b67cba00d4794b024b2887e448c9
ODiogoSilva <o.diogosilva@gmail.com>
2016-11-03 15:01:00 +0000

    Fixed issue with path interactive reference in secondary filechoosers

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ mcl_file = mcl_data_files()
 
 setup(
     name="TriFusion",
-    version="0.4.49-1",
+    version="0.4.50-1",
     packages=["trifusion",
               "trifusion.base",
               "trifusion.data",
