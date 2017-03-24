402cbc9311339616bf4e4f39c3de088e4b9e3420
ODiogoSilva <o.diogosilva@gmail.com>
2017-03-24 00:08:47 +0000

    finished triseq tests
    Former-commit-id: 463d289f54d70d8c81041331c70937eccd363267 [formerly b9932309ccb2eb6c0ca8ea02d986ef9b6ce24905] [formerly 344a40c5e5e75fc0e77ce825f7fcbad529e4becf]
    Former-commit-id: 433c41e9bf64c038a8873e93c425bd04d53da1d9
    Former-commit-id: fc9cdaa8f96ce7132e9b9a18f80941697393ae9f

--- trifusion/tests/test_triseq.py
+++ trifusion/tests/test_triseq.py
@@ -8,12 +8,16 @@ from os.path import join
 from data_files import *
 import shutil
 
+from argparse import ArgumentTypeError, ArgumentError
+
 try:
     from process.sequence import AlignmentList, Alignment
-    from TriSeq import get_args, main
+    from TriSeq import get_args, main, triseq_arg_check
+    from base.sanity import triseq_arg_check
 except ImportError:
     from trifusion.process.sequence import AlignmentList, Alignment
     from trifusion.TriSeq import get_args, main
+    from trifusion.base.sanity import triseq_arg_check
 
 output_dir = "triseq_test"
 data_path = join("trifusion/tests/data/")
@@ -34,9 +38,9 @@ class TriSeqTest(unittest.TestCase):
 
         args = get_args(["-in"] + dna_data_fas +
                         ["-of", "fasta",
-                        "-o", join(output_dir, "teste"),
-                        "-quiet"])
-
+                         "-o", join(output_dir, "teste"),
+                         "-quiet"])
+        triseq_arg_check(args)
         main(args)
 
         with open(join(data_path, "BaseConcatenation.fas")) as fh1, \
@@ -46,10 +50,10 @@ class TriSeqTest(unittest.TestCase):
     def test_simple_conversion(self):
 
         args = get_args(["-in", dna_data_fas[0],
-                        "-of", "fasta",
+                         "-of", "fasta",
                          "-c",
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         with open(join(data_path, "BaseConc1.fas")) as fh1, \
@@ -64,7 +68,7 @@ class TriSeqTest(unittest.TestCase):
                          "-r", concatenated_small_par[0],
                          "-of", "fasta",
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         exp = []
@@ -87,7 +91,7 @@ class TriSeqTest(unittest.TestCase):
         args = get_args(["-p", concatenated_small_par[0],
                          "-o", join(output_dir, "test"),
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         with open(concatenated_small_parNex[0]) as fh1, \
@@ -99,19 +103,32 @@ class TriSeqTest(unittest.TestCase):
         args = get_args(["-p", concatenated_small_parNex[0],
                          "-o", join(output_dir, "test"),
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         with open(concatenated_small_par[0]) as fh1, \
                 open(join(output_dir, "test.part.File")) as fh2:
             self.assertEqual(fh1.read(), fh2.read().replace("LG", "DNA"))
 
+    def test_convert_raxml2nex_with_model(self):
+
+        args = get_args(["-p", concatenated_small_parNex[0],
+                         "-o", join(output_dir, "test"),
+                         "--model", "WAG",
+                         "-quiet"])
+        triseq_arg_check(args)
+        main(args)
+
+        with open(concatenated_small_par[0]) as fh1, \
+                open(join(output_dir, "test.part.File")) as fh2:
+            self.assertEqual(fh1.read(), fh2.read().replace("WAG", "DNA"))
+
     def test_select_alignments(self):
 
         args = get_args(["-in"] + dna_data_fas +
                         ["-s", "spa",
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         fls = sorted(os.listdir("Taxa_selection"))
@@ -127,12 +144,212 @@ class TriSeqTest(unittest.TestCase):
                          "-o", join(output_dir, "teste"),
                          "-of", "fasta",
                          "-quiet"])
-
+        triseq_arg_check(args)
         main(args)
 
         fls = sorted(os.listdir(output_dir))
 
         self.assertEqual(fls, ['teste.fas', 'teste.haplotypes'])
+
+    def test_code_gaps(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--code-gaps",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+        triseq_arg_check(args)
+        main(args)
+
+
+    def test_consensus(self):
+
+        args = get_args(["-in", dna_data_fas[0],
+                        "--consensus", "Soft mask",
+                         "-of", "fasta",
+                         "-c",
+                         "-quiet"])
+        triseq_arg_check(args)
+        main(args)
+
+        with open("BaseConc1.fas") as fh:
+            self.assertTrue(fh.read().startswith(">consensus"))
+
+        os.remove("BaseConc1.fas")
+
+    def test_consensus_singlefile(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--consensus", "Soft mask",
+                         "--consensus-single-file",
+                         "-of", "fasta",
+                         "-c",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+
+        self.assertTrue(os.path.exists("consensus.fas"))
+        os.remove("consensus.fas")
+
+    def test_missing_filter_perc_val(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--missing-filter", "10", "50",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_missing_filter_prop_val(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--missing-filter", "0.1", "0.5",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_min_taxa_perc_val(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--min-taxa", "50",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_min_taxa_prop_val(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--min-taxa", "0.5",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_contain_taxa(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--contain-taxa", "spa",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_exclude_taxa(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--exclude-taxa", "spa",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_codon_filter(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--codon-filter", "1", "2",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_variable_filter(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--variable-filter", "1", "2",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_interleave(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--interleave",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.fas")))
+
+    def test_ima2_output(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--ima2-params", ima2_pop_file, "(1,2):3)4:5", "IS",
+                         "1",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "ima2",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        self.assertTrue(os.path.exists(join(output_dir, "teste.txt")))
+
+    def test_remove_taxa(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["-rm", "spa",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        with open(join(output_dir, "teste.fas")) as fh:
+            self.assertEqual(fh.read().count("spa"), 0)
+
+    def test_grep_taxa(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["-grep", "spa",
+                         "-o", join(output_dir, "teste"),
+                         "-of", "fasta",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+        with open(join(output_dir, "teste.fas")) as fh:
+            self.assertEqual(fh.read().count("spa"), 1)
+
+    def test_get_taxa(self):
+
+        args = get_args(["-in"] + dna_data_fas +
+                        ["--get-taxa",
+                         "-quiet"])
+
+        triseq_arg_check(args)
+        main(args)
+
+        self.assertTrue(os.path.exists("Taxa_list.csv"))
+        os.remove("Taxa_list.csv")
 
 
 if __name__ == "__main__":
