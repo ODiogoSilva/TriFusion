f47156b39dca4c3f11c988acf577644056faf7a3
ODiogoSilva <o.diogosilva@gmail.com>
2017-04-13 13:09:32 +0100

    Added general purposes boxes to theme

--- trifusion/data/resources/theme/default.py
+++ trifusion/data/resources/theme/default.py
@@ -12,3 +12,9 @@ c_outer_side_panel_background = (0.1, 0.1, 0.1, .9)
 
 # Sidepanel Caroussel headers
 c_side_panel_headers = (.3, .3, .3, 1)
+
+# Light boxes
+c_light_box = (.2, .2, .2, 1)
+
+# Dark boxes
+c_dark_box = (0, 0, 0, 0)
