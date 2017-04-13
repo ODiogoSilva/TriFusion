72f4bbd5e4ffee4a8dea32fb33148d8a3b289246
ODiogoSilva <o.diogosilva@gmail.com>
2017-04-13 10:50:02 +0100

    started with theme for main backgrounds

---
+++ trifusion/data/resources/theme/default.py
@@ -0,0 +1,10 @@
+"""
+This file contains the color scheme for some widgets and backgrounds of
+TriFusion
+"""
+
+# Main window background
+c_main_background = (.08, .08, .08, 1)
+
+# Sidepanel background
+c_side_panel_background = (.1, .1, .1, .8)
