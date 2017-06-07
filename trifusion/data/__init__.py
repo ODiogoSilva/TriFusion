c7fc6c09c01798ec55fbef8b833497c4f332f3e8
ODiogoSilva <o.diogosilva@gmail.com>
2017-06-07 16:15:05 +0100

    Added module list to data subpackage #268

--- trifusion/data/__init__.py
+++ trifusion/data/__init__.py
@@ -13,7 +13,16 @@ Stores all the backgrounds and icons of TriFusion.
 
 Stores several resources used by TriFusion, including custom widgets,
 definition of background (worker) tasks, information data for in-app help,
-and auxiliary statistics data structures.
+and auxiliary statistics data structures:
+
+    - :mod:`~trifusion.data.resources.background_tasks`: Includes all
+      TriFusion tasks that are executed on the background.
+    - :mod:`~trifusion.data.resources.info_data`: Contains dictionary
+      objects with the in-app help text.
+    - :mod:`~trifusion.data.resources.custom_widgets`: Contains class
+      definition of custom widgets used in TriFusion.
+    - :mod:`~trifusion.data.resources.stats`: Dictionary with information
+      for the Statistics FloatLayout with the plot type switch.
 
 It also contains the bundled MCL binary for all supported OS and the usearch
 .dll file, which are required by TriFusion's Orthology.
